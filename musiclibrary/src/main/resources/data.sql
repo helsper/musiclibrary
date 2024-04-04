9e179c027a0819509a3286d0fb111cb0259e2527
helsper <166015941+helsper@users.noreply.github.com>
2024-04-04 14:21:52 +0200

    added default data

---
+++ musiclibrary/src/main/resources/data.sql
@@ -0,0 +1,5 @@
+INSERT INTO album (name, artist, release_date, number_of_songs, genre, description, image, created, updated)
+VALUES
+    ('Album 1', 'Artist 1', '2023-01-01', 12, 'POP', 'Description for Album 1', NULL, '2024-04-04', '2024-04-04'),
+    ('Album 2', 'Artist 2', '2022-05-15', 10, 'ROCK', 'Description for Album 2', NULL, '2024-04-04', '2024-04-04'),
+    ('Album 3', 'Artist 3', '2024-02-20', 15, 'HIP_HOP', 'Description for Album 3', NULL, '2024-04-04', '2024-04-04');
