7f471fac2808bfc7f987fb0b5a9f71cf469cee4a
helsper <166015941+helsper@users.noreply.github.com>
2024-04-05 00:32:29 +0200

    implemented AlbumController for all necessary Crud Operations

---
+++ musiclibrary/src/main/java/de/helsper/musiclibrary/album/controller/AlbumController.java
@@ -0,0 +1,33 @@
+package de.helsper.musiclibrary.album.controller;
+
+import de.helsper.musiclibrary.album.entity.model.Album;
+import de.helsper.musiclibrary.album.service.AlbumService;
+import lombok.AccessLevel;
+import lombok.RequiredArgsConstructor;
+import lombok.experimental.FieldDefaults;
+import org.springframework.web.bind.annotation.*;
+
+
+@RestController
+@RequiredArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE)
+@RequestMapping("albums")
+public class AlbumController {
+
+    final AlbumService albumService;
+
+    @PostMapping
+    public Album create(@RequestBody Album album) { return albumService.create(album); }
+
+    @GetMapping
+    public Iterable<Album> findAll() { return albumService.findAll(); }
+
+    @GetMapping("{id}")
+    public Album findById(@PathVariable Long id) { return albumService.findById(id); }
+
+    @PutMapping("{id}")
+    public Album update(@PathVariable Long id, @RequestBody Album album) { return albumService.update(id, album); }
+
+    @DeleteMapping("{id}")
+    public void delete(@PathVariable Long id) { albumService.deleteById(id); }
+}
